7d8f4cb36f31ed579d12799f4e1af95bc668bd74
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-13 00:02:49 +0300

    day-4

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const listContainer = document.querySelector('.container');
-const section = document.createElement('section');
-const ul = document.createElement('ul');
-const li = document.createElement('li');
+
+const createElement = () => {
+  const section = document.createElement('section');
+  const h4 = document.createElement('h4');
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+
+  // styles
+  const sectionClassList = ['d-flex', 'justify-content-center', 'flex-column', 'my-4', 'lists', 'bg-dark', 'py-5'];
+  const h4ClassList = ['d-flex', 'justify-content-center', 'py-4', 'text-warning']
+  const ulClassList = ['nes-list', 'is-disc'];
+
+  //  define that styles to the elements
+  section.classList.add(...sectionClassList);
+  h4.classList.add(...h4ClassList);
+  ul.classList.add(...ulClassList);
+
+  // append elements to dom
+  listContainer.appendChild(section);
+  section.appendChild(h4);
+  section.appendChild(ul);
+
+  // li elements
+  let list = [{
+    value: 'a',
+  }, {
+    value: 'b',
+  }, {
+    value: 'c'
+  }, {
+    value: 'd'
+  }]
+  let day = "x";
+
+  // h4 
+  h4.innerHTML = `day-${day}`
 
 
-section.classList.add('d-flex')
+  // map fonksiyonu ile li elemanlarini ul ye appen ediyoruz
 
-listContainer.appendChild(section);
-// d-flex justify-content-center my-4 lists bg-dark py-5
+  list.map(({
+    value
+  }) => {
+    return ul.innerHTML += `<li class="py-3">${value}</li>`
+  });
+
+}
+
+
+createElement()
+
+// javascript destructuring
