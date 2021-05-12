cafc1b30ba752a35f2fad2b7dc90e20df5ce2527
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-12 14:04:22 +0300

    scripts added

---
+++ index.js
@@ -0,0 +1,10 @@
+const listContainer = document.querySelector('.container');
+const section = document.createElement('section');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+
+
+section.classList.add('d-flex')
+
+listContainer.appendChild(section);
+// d-flex justify-content-center my-4 lists bg-dark py-5
